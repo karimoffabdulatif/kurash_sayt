ae8dea4f109bc5d70d382fa7a3c9fbac3c53e97a
karimoffabdulatif <akarimoff001@gmail.com>
2026-03-11 02:45:57 +0500

    push

--- app/components/MainSection.jsx
+++ app/components/MainSection.jsx
@@ -2,9 +2,15 @@ export default function MainSection() {
   return (
     <section className="py-20">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <h2 className="text-4xl font-semibold mb-6 tracking-widest">WBK&BOKA</h2>
-        <p className="text-lg w-[900px] text-gray-700">
-          Dunyo Belbog‘li Kurash Federatsiyasi – kurashni rivojlantirish, xalqaro turnirlar tashkil etish va sportchilarni qo‘llab-quvvatlashga bag‘ishlangan global tashkilot.
+        {/* Sarlavha */}
+        <h2 className="text-3xl sm:text-4xl md:text-5xl font-semibold mb-6 tracking-widest text-center sm:text-left">
+          WBK&BOKA
+        </h2>
+
+        {/* Matn */}
+        <p className="text-base text-center sm:text-lg md:text-lg text-gray-700 max-w-full sm:max-w-[700px] md:max-w-[900px] mx-auto sm:mx-0">
+          Dunyo Belbog‘li Kurash Federatsiyasi – kurashni rivojlantirish, xalqaro turnirlar
+          tashkil etish va sportchilarni qo‘llab-quvvatlashga bag‘ishlangan global tashkilot.
         </p>
       </div>
     </section>
